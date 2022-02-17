06a4c23488606fe6484e45ad5a21e228f1eb79ea
SirCipher <tklapwijk92@gmail.com>
2022-02-17 14:27:52 +0000

    Removes unused code

--- swim-java/src/main/java/ai/swim/RustRuntime.java
+++ swim-java/src/main/java/ai/swim/RustRuntime.java
@@ -19,8 +19,6 @@ public class RustRuntime {
   private static final long TOKIO_HANDLE;
 
   static {
-    System.out.println(System.getProperty("user.dir"));
-
     System.loadLibrary("core_ffi");
 
     TOKIO_HANDLE = initRuntime();
