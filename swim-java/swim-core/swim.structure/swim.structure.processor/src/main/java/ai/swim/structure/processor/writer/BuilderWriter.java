369b4ba1146383f7f02790b2baa97bce688cb962
SirCipher <tklapwijk92@gmail.com>
2022-05-04 08:08:56 +0100

    Feed index writing

--- swim-java/swim-core/swim.structure/swim.structure.processor/src/main/java/ai/swim/structure/processor/writer/BuilderWriter.java
+++ swim-java/swim-core/swim.structure/swim.structure.processor/src/main/java/ai/swim/structure/processor/writer/BuilderWriter.java
@@ -21,44 +21,83 @@ import static ai.swim.structure.processor.writer.RecognizerWriter.TYPE_READ_EVEN
 
 public class BuilderWriter {
   private static final String FIELD_RECOGNIZING_BUILDER_CLASS = "ai.swim.structure.FieldRecognizingBuilder";
+  private static final String RECOGNIZING_BUILDER_CLASS = "ai.swim.structure.RecognizingBuilder";
   private static final String RECOGNIZING_BUILDER_FEED_INDEX = "feedIndexed";
   private static final String RECOGNIZING_BUILDER_BIND = "bind";
 
   public static void write(ClassSchema schema, ScopedContext context) throws IOException {
     ProcessingEnvironment processingEnvironment = context.getProcessingContext().getProcessingEnvironment();
+    Types typeUtils = processingEnvironment.getTypeUtils();
+    Elements elementUtils = processingEnvironment.getElementUtils();
+
     TypeSpec.Builder classSpec = TypeSpec.classBuilder(schema.className() + "Builder").addModifiers(Modifier.PUBLIC, Modifier.FINAL);
+
+    TypeElement recognizingBuilderElement = elementUtils.getTypeElement(RECOGNIZING_BUILDER_CLASS);
+    DeclaredType recognizingBuilderType = typeUtils.getDeclaredType(recognizingBuilderElement, context.getRoot().asType());
+
+    classSpec.addSuperinterface(TypeName.get(recognizingBuilderType));
 
     classSpec.addMethod(buildConstructor());
     classSpec.addFields(Arrays.asList(buildFields(schema, processingEnvironment)));
-    classSpec.addMethods(Arrays.asList(buildMethods(schema, processingEnvironment)));
+    classSpec.addMethods(Arrays.asList(buildMethods(schema, context)));
 
     JavaFile javaFile = JavaFile.builder(schema.getPackageElement().getQualifiedName().toString(), classSpec.build()).build();
     javaFile.writeTo(System.out);
   }
 
-  private static MethodSpec[] buildMethods(ClassSchema schema, ProcessingEnvironment processingEnvironment) {
+  private static MethodSpec[] buildMethods(ClassSchema schema, ScopedContext context) {
     MethodSpec[] methods = new MethodSpec[2];
-    methods[0] = buildFeedIndexed(schema, processingEnvironment);
-    methods[1] = buildBind(schema, processingEnvironment);
+    methods[0] = buildFeedIndexed(schema, context.getProcessingContext().getProcessingEnvironment());
+    methods[1] = buildBind(schema, context);
 
     return methods;
   }
 
-  private static MethodSpec buildBind(ClassSchema schema, ProcessingEnvironment processingEnvironment) {
-    Elements elementUtils = processingEnvironment.getElementUtils();
-    Types typeUtils = processingEnvironment.getTypeUtils();
-
-    TypeElement readEventType = elementUtils.getTypeElement(TYPE_READ_EVENT);
-
-    MethodSpec.Builder builder = MethodSpec.methodBuilder(RECOGNIZING_BUILDER_FEED_INDEX)
-        .addParameter(Integer.TYPE, "index")
-        .addParameter(TypeName.get(readEventType.asType(), "event");
-
-
-    return null;
+  private static String buildFieldName(String fieldName) {
+    return fieldName + "Builder";
   }
 
   private static MethodSpec buildFeedIndexed(ClassSchema schema, ProcessingEnvironment processingEnvironment) {
+    Elements elementUtils = processingEnvironment.getElementUtils();
+    TypeElement readEventType = elementUtils.getTypeElement(TYPE_READ_EVENT);
+    MethodSpec.Builder builder = MethodSpec.methodBuilder(RECOGNIZING_BUILDER_FEED_INDEX)
+        .addModifiers(Modifier.PUBLIC)
+        .addAnnotation(Override.class)
+        .addParameter(Integer.TYPE, "index")
+        .addParameter(TypeName.get(readEventType.asType()), "event")
+        .returns(boolean.class);
+
+    CodeBlock.Builder body = CodeBlock.builder();
+    body.beginControlFlow("switch (index)");
+
+    List<ElementRecognizer> recognizers = schema.getRecognizers();
+
+    for (int i = 0; i < recognizers.size(); i++) {
+      ElementRecognizer recognizer = recognizers.get(i);
+      System.out.println("Writing: " + recognizer.fieldName());
+
+      body.add("case $L:", i);
+      body.addStatement("\nreturn this.$L.feed(event)", buildFieldName(recognizer.fieldName()));
+
+      System.out.println("Wrote: " + recognizer.fieldName());
+    }
+
+    body.add("default:").addStatement("\nthrow new RuntimeException(\"Unknown idx: \" + index)").endControlFlow();
+    builder.addCode(body.build());
+
+    return builder.build();
+  }
+
+  private static MethodSpec buildBind(ClassSchema schema, ScopedContext context) {
+    ProcessingEnvironment processingEnvironment = context.getProcessingContext().getProcessingEnvironment();
+    Elements elementUtils = processingEnvironment.getElementUtils();
+
+    TypeElement readEventType = elementUtils.getTypeElement(TYPE_READ_EVENT);
+    MethodSpec.Builder builder = MethodSpec.methodBuilder(RECOGNIZING_BUILDER_FEED_INDEX)
+        .addModifiers(Modifier.PUBLIC)
+        .addAnnotation(Override.class)
+        .returns(TypeName.get(context.getRoot().asType()));
+
     return null;
   }
 
@@ -86,7 +125,7 @@ public class BuilderWriter {
       }
 
       DeclaredType memberRecognizingBuilder = typeUtils.getDeclaredType(fieldFieldRecognizingBuilder, recognizerType);
-      FieldSpec.Builder fieldSpec = FieldSpec.builder(TypeName.get(memberRecognizingBuilder), recognizer.fieldName() + "Builder", Modifier.PRIVATE, Modifier.FINAL);
+      FieldSpec.Builder fieldSpec = FieldSpec.builder(TypeName.get(memberRecognizingBuilder), buildFieldName(recognizer.fieldName()), Modifier.PRIVATE, Modifier.FINAL);
 
       System.out.println("Writing code block with init: " + recognizer.initializer());
 
