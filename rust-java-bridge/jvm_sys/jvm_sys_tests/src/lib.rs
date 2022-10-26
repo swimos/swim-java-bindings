59459b8f65f8e04759e309d1724f9923b793b787
SirCipher <tklapwijk92@gmail.com>
2022-10-26 15:09:04 +0100

    Init old buffer

---
+++ rust-java-bridge/jvm_sys/jvm_sys_tests/src/lib.rs
@@ -0,0 +1,204 @@
+// Copyright 2015-2022 Swim Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+///! Unit tests invoked by swim-bridge unit tests.
+use std::future::Future;
+use std::io::ErrorKind;
+
+use bytes::BytesMut;
+use jni::errors::Error;
+use jni::objects::{JByteBuffer, JClass};
+use jni::sys::{jbyteArray, jint, jobject};
+use jni::JNIEnv;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::runtime::{Builder, Runtime};
+
+use jvm_sys::channel::reader::ByteReader;
+use jvm_sys::channel::writer::ByteWriter;
+use jvm_sys::vm::method::InvokeObjectMethod;
+use jvm_sys::vm::method::JavaMethod;
+use jvm_sys::vm::utils::get_env;
+use sys_util::{jvm_tryf, npch};
+
+/// Creates a new multi-threaded Tokio runtime and spawns 'fut' on to it. A monitor task is spawned
+/// that waits for 'fut' to complete and then notifies 'barrier' that the task has completed; this
+/// task must be run as its own Tokio task in case 'fut' panics as it is not possible to catch an
+/// unwind due to uses of &mut T by readers and writers.
+fn run_test<F>(env: JNIEnv, barrier: jobject, fut: F) -> *mut Runtime
+where
+    F: Future + Send + 'static,
+    F::Output: Send + 'static,
+{
+    npch!(env, barrier);
+
+    let vm = env.get_java_vm().unwrap();
+    let global_ref = env.new_global_ref(barrier).unwrap();
+
+    let runtime = Builder::new_multi_thread()
+        .build()
+        .expect("Failed to build runtime");
+
+    let join_handle = runtime.spawn(fut);
+    runtime.spawn(async move {
+        let r = join_handle.await;
+        let env = get_env(&vm).unwrap();
+
+        let _guard = env.lock_obj(&global_ref).expect("Failed to enter monitor");
+        let countdown = JavaMethod::new("countDown", "()V");
+
+        match countdown.invoke(&env, &global_ref, &[]) {
+            Ok(_) => {}
+            Err(Error::JavaException) => {
+                let throwable = env.exception_occurred().unwrap();
+                jvm_tryf!(env, env.throw(throwable));
+            }
+            Err(e) => env.fatal_error(&e.to_string()),
+        }
+        if r.is_err() {
+            env.fatal_error("Test panicked");
+        }
+    });
+
+    Box::into_raw(Box::new(runtime))
+}
+
+#[no_mangle]
+pub extern "system" fn Java_ai_swim_bridge_channel_FfiChannelTest_readerTask(
+    env: JNIEnv,
+    _class: JClass,
+    bb: JByteBuffer,
+    monitor: jobject,
+    buf: jbyteArray,
+    barrier: jobject,
+) -> *mut Runtime {
+    npch!(env, bb, monitor, buf);
+
+    let expected = env.convert_byte_array(buf).unwrap();
+    let mut reader = ByteReader::new(env, bb, monitor);
+
+    run_test(env, barrier, async move {
+        let mut buf = BytesMut::new();
+        buf.reserve(buf.len() * 2);
+
+        loop {
+            match reader.read_buf(&mut buf).await {
+                Ok(_) => {}
+                Err(e) if e.kind() == ErrorKind::BrokenPipe => {
+                    let slice = buf.as_ref();
+                    let expected_len = expected.len();
+                    let actual_len = slice.len();
+
+                    if expected.len() == slice.len() {
+                        assert_eq!(&slice[..expected.len()], expected.as_slice());
+                        assert!(!slice[expected.len()..].iter().any(|e| *e == 0));
+                        break;
+                    } else {
+                        panic!(
+                            "Expected ({}) and actual ({}) buffer lengths differ",
+                            expected_len, actual_len
+                        );
+                    }
+                }
+                Err(e) => panic!("{:?}", e),
+            }
+        }
+    })
+}
+
+#[no_mangle]
+pub extern "system" fn Java_ai_swim_bridge_channel_FfiChannelTest_writerTask(
+    env: JNIEnv,
+    _class: JClass,
+    bb: JByteBuffer,
+    monitor: jobject,
+    buf: jbyteArray,
+    chunk_size: jint,
+    barrier: jobject,
+) -> *mut Runtime {
+    npch!(env, bb, monitor, buf);
+
+    let mut to_write = env.convert_byte_array(buf).unwrap();
+    let mut writer = ByteWriter::new(env, bb, monitor);
+
+    run_test(env, barrier, async move {
+        for chunk in to_write.chunks_mut(chunk_size as usize) {
+            if let Err(e) = writer.write_all(chunk).await {
+                panic!("Unexpected write failure: {:?}", e);
+            }
+        }
+    })
+}
+
+#[no_mangle]
+pub extern "system" fn Java_ai_swim_bridge_channel_FfiChannelTest_writerClosedTask(
+    env: JNIEnv,
+    _class: JClass,
+    bb: JByteBuffer,
+    monitor: jobject,
+    barrier: jobject,
+) -> *mut Runtime {
+    npch!(env, bb, monitor);
+
+    let mut writer = ByteWriter::new(env, bb, monitor);
+
+    run_test(env, barrier, async move {
+        match writer.write_all(&[1, 2, 3, 4, 5]).await {
+            Ok(_) => {
+                panic!("Unexpected write")
+            }
+            Err(e) if e.kind() == ErrorKind::BrokenPipe => {}
+            Err(e) => panic!("Unexpected error: {:?}", e),
+        }
+    })
+}
+
+#[no_mangle]
+pub extern "system" fn Java_ai_swim_bridge_channel_FfiChannelTest_dropWriterTask(
+    env: JNIEnv,
+    _class: JClass,
+    bb: JByteBuffer,
+    monitor: jobject,
+    barrier: jobject,
+) -> *mut Runtime {
+    npch!(env, bb, monitor);
+
+    let writer = ByteWriter::new(env, bb, monitor);
+    run_test(env, barrier, async move {
+        drop(writer);
+    })
+}
+
+#[no_mangle]
+pub extern "system" fn Java_ai_swim_bridge_channel_FfiChannelTest_dropReaderTask(
+    env: JNIEnv,
+    _class: JClass,
+    bb: JByteBuffer,
+    monitor: jobject,
+    barrier: jobject,
+) -> *mut Runtime {
+    npch!(env, bb, monitor);
+
+    let reader = ByteReader::new(env, bb, monitor);
+    run_test(env, barrier, async move { drop(reader) })
+}
+
+#[no_mangle]
+#[allow(clippy::not_unsafe_ptr_arg_deref, clippy::missing_safety_doc)]
+pub unsafe extern "system" fn Java_ai_swim_bridge_channel_FfiChannelTest_dropRuntime(
+    _env: JNIEnv,
+    _class: JClass,
+    runtime: *mut Runtime,
+) {
+    drop(Box::from_raw(runtime));
+}
---
+++ rust-java-bridge/jvm_sys/src/channel/mod.rs
@@ -0,0 +1,175 @@
+// Copyright 2015-2022 Swim Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! JNI boundary byte channels.
+//!
+//! # Layout/synchronization
+//! A channel is backed by a single buffer that contains a closed flag (u32), read index (u32),
+//! write index (u32) and then data (u8). This results in 12 extra bytes being required in addition
+//! to the requested channel capacity by the user. Only operations on the closed flag are atomic
+//! and all other get/set operations are done behind synchronization using Java monitors.
+//!
+//! On both the Java and Rust side, when a reader/writer wants to perform an operation a monitor
+//! must be acquired on the provided lock object before any access is granted to the shared buffer.
+//! Following the operation taking place, the lock object is notified and the monitor is
+//! relinquished.
+//!
+//! For Java this is:
+//! ```ignore
+//! synchronized(lock) {
+//!     // do buffer work
+//!     lock.notify();
+//! }
+//! ```
+//!
+//! For Rust this is:
+//! ```ignore
+//! let monitor = env.lock_obj(&self.lock).expect("Failed to enter monitor");
+//! // do buffer work
+//! jvm_tryf!(env, JavaMethod::NOTIFY.invoke(&env, &self.lock, &[]));
+//! // monitor is released when it is dropped
+//! ```
+
+use std::fmt::{Debug, Formatter};
+use std::mem::size_of;
+use std::ptr::NonNull;
+use std::sync::atomic::{AtomicBool, AtomicI32, Ordering};
+
+use jni::objects::{GlobalRef, JByteBuffer};
+use jni::sys::jobject;
+use jni::{JNIEnv, JavaVM};
+use tokio::io::ReadBuf;
+
+use sys_util::jvm_tryf;
+
+use crate::bytes::Bytes;
+use crate::vm::method::InvokeObjectMethod;
+use crate::vm::method::JavaMethod;
+use crate::vm::utils::get_env;
+
+pub mod reader;
+pub mod writer;
+mod b2;
+
+mod offset {
+    use std::mem::size_of;
+
+    #[cfg(target_endian = "little")]
+    pub const CLOSED: usize = 0;
+    #[cfg(target_endian = "big")]
+    pub const CLOSED: usize = 3;
+    pub const READ: usize = size_of::<i32>();
+    pub const WRITE: usize = 2 * size_of::<i32>();
+    pub const DATA_START: usize = 3 * size_of::<i32>();
+}
+
+struct Inner {
+    pub is_closed: NonNull<AtomicBool>,
+    pub read_index: NonNull<AtomicI32>,
+    pub write_index: NonNull<AtomicI32>,
+    pub lock: GlobalRef,
+    pub bytes: Bytes,
+    pub vm: JavaVM,
+}
+
+unsafe impl Send for Inner {}
+unsafe impl Sync for Inner {}
+
+impl Debug for Inner {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ByteWriter")
+            .field("is_closed", &self.is_closed())
+            .field("read_index", unsafe {
+                &self.read_index.as_ref().load(Ordering::Relaxed)
+            })
+            .field("write_index", unsafe {
+                &self.write_index.as_ref().load(Ordering::Relaxed)
+            })
+            .field("bytes", &"...")
+            .field("vm", &"..")
+            .finish()
+    }
+}
+
+impl Drop for Inner {
+    fn drop(&mut self) {
+        unsafe { self.is_closed.as_ref().store(true, Ordering::Relaxed) };
+
+        let env = get_env(&self.vm).expect("Failed to get JVM environment");
+        let _guard = env.lock_obj(&self.lock).expect("Failed to enter monitor");
+
+        jvm_tryf!(env, JavaMethod::NOTIFY.invoke(&env, &self.lock, &[]));
+    }
+}
+
+fn make_atomic_i32(bytes: &Bytes, offset: usize) -> NonNull<AtomicI32> {
+    const SIZE: usize = size_of::<i32>();
+    unsafe {
+        NonNull::new_unchecked(bytes.slice_mut(offset, SIZE) as *const [u8; SIZE] as *mut AtomicI32)
+    }
+}
+
+impl Inner {
+    pub fn new(env: JNIEnv, buffer: JByteBuffer, lock: jobject) -> Inner {
+        let bytes = Bytes::new(env, buffer);
+        let global_ref = env
+            .new_global_ref(lock)
+            .expect("Failed to get a global reference to byte channel lock");
+
+        Inner {
+            is_closed: unsafe { NonNull::new_unchecked(bytes.get_mut_u8(offset::CLOSED) as _) },
+            read_index: make_atomic_i32(&bytes, offset::READ),
+            write_index: make_atomic_i32(&bytes, offset::WRITE),
+            lock: global_ref,
+            bytes,
+            vm: jvm_tryf!(env, env.get_java_vm()),
+        }
+    }
+
+    pub fn is_closed(&self) -> bool {
+        unsafe { self.is_closed.as_ref().load(Ordering::Relaxed) }
+    }
+
+    pub fn data_capacity(&self) -> usize {
+        self.bytes.capacity() - offset::DATA_START
+    }
+}
+
+pub trait ReadTarget {
+    fn remaining(&self) -> usize;
+
+    fn put_slice(&mut self, slice: &[u8]);
+}
+
+#[derive(Debug)]
+pub enum WriteFailure {
+    Closed,
+    Full,
+}
+
+#[derive(Debug)]
+pub enum ReadFailure {
+    Closed,
+    Empty,
+}
+
+impl<'a> ReadTarget for ReadBuf<'a> {
+    fn remaining(&self) -> usize {
+        ReadBuf::remaining(self)
+    }
+
+    fn put_slice(&mut self, slice: &[u8]) {
+        ReadBuf::put_slice(self, slice)
+    }
+}
